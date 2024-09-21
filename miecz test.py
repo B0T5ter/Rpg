ce2c1551790c229b484d0bbde2176a69846a15d7
Filip <targosz.f@gmail.com>
2024-09-21 13:43:33 +0200

    Mieczyk się obraca a postać jest kółkiem

--- miecz test.py
+++ miecz test.py
@@ -5,20 +5,45 @@ windowHeight = 400
 windowWidth = 500
 
 screen = pygame.display.set_mode((windowWidth,windowHeight))
-mieczpng = pygame.image.load("texture\miecz.png")
+
 angle = 0
 zasieg = 100
+class Miecz:
+    def __init__(self, x, y, angle, image = pygame.image.load("texture\miecz.png")) -> None:
+        self.x = x
+        self.y = y
+        self.image = image
+        self.image_rotated = image
+        self.angle = angle
+    
+    def draw(self):
+        screen.blit(self.image_rotated, (self.x  ,self.y))
+
+    def obrot_miecza(self):
+        key = pygame.key.get_pressed()
+        if key[pygame.K_w] == True:
+            self.angle = 0
+        if key[pygame.K_d] == True:
+            self.angle = 270
+        if key[pygame.K_s] == True:
+            self.angle = 180
+        if key[pygame.K_a] == True:
+            self.angle = 90
+
+        self.image_rotated = pygame.transform.rotate(self.image, self.angle)
+
+miecz = Miecz(100,100, 0)      
 while True:
     screen.fill('white')
-    mieczpng = pygame.transform.rotate(mieczpng, angle)
-    pygame.draw.rect(screen, ("black"), pygame.Rect(windowWidth/2 - 25 , windowHeight/2 - 25, 50, 50))
+    pygame.draw.circle(screen, ("black"), (windowWidth/2 , windowHeight/2), 25)
+    #pygame.draw.rect(screen, ("black"), pygame.Rect(windowWidth/2 - 25 , windowHeight/2 - 25, 50, 50))
+    miecz.obrot_miecza()
+    miecz.draw()  
     
-    mieczx = windowWidth//2 + math.cos(angle) * zasieg
-    mieczy = windowWidth//2 + math.sin(angle) * zasieg
-    print(mieczx ,mieczy)
-    screen.blit(mieczpng, (200  ,200))
     
-    angle += 0.05
+    #mieczx = windowWidth//2 + math.cos(angle) * zasieg
+    #mieczy = windowWidth//2 + math.sin(angle) * zasieg
+    
     
     
     for event in pygame.event.get():
