d592764108711ff82ab3ced9babaadf0dd78bec3
Filip <targosz.f@gmail.com>
2024-09-21 14:07:22 +0200

    Mieczyk działa teraz pora na animacje

--- miecz test.py
+++ miecz test.py
@@ -10,11 +10,13 @@ angle = 0
 zasieg = 100
 class Miecz:
     def __init__(self, x, y, angle, image = pygame.image.load("texture\miecz.png")) -> None:
-        self.x = x
-        self.y = y
+        self.x = 260
+        self.y = 150
         self.image = image
         self.image_rotated = image
         self.angle = angle
+        self.animacja = [pygame.image.load("texture\animacjamiecza1.png"), pygame.image.load("texture\animacjamiecza2.png"),pygame.image.load("texture\animacjamiecza3.png")]
+        self.klatka_animacji = 0
     
     def draw(self):
         screen.blit(self.image_rotated, (self.x  ,self.y))
@@ -23,28 +25,30 @@ class Miecz:
         key = pygame.key.get_pressed()
         if key[pygame.K_w] == True:
             self.angle = 0
+            self.x = 260  
+            self.y = 150
         if key[pygame.K_d] == True:
             self.angle = 270
+            self.x = 250  
+            self.y = 205
         if key[pygame.K_s] == True:
             self.angle = 180
+            self.x = 190
+            self.y = 190
         if key[pygame.K_a] == True:
             self.angle = 90
-
+            self.x = 200
+            self.y = 140
+        
         self.image_rotated = pygame.transform.rotate(self.image, self.angle)
 
 miecz = Miecz(100,100, 0)      
 while True:
     screen.fill('white')
     pygame.draw.circle(screen, ("black"), (windowWidth/2 , windowHeight/2), 25)
-    #pygame.draw.rect(screen, ("black"), pygame.Rect(windowWidth/2 - 25 , windowHeight/2 - 25, 50, 50))
     miecz.obrot_miecza()
     miecz.draw()  
-    
-    
-    #mieczx = windowWidth//2 + math.cos(angle) * zasieg
-    #mieczy = windowWidth//2 + math.sin(angle) * zasieg
-    
-    
+     
     
     for event in pygame.event.get():
          if event.type == pygame.QUIT:
